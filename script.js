e80e63aa596666d36e51c54c8c70480fbc219189
Eric Chavez <eric@emcaudio.com>
2019-09-08 08:09:22 -0500

    revamped

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function makeSelected(selected){
+    console.log(selected)
     let tabs = document.getElementsByClassName('navItem')
     for(let tab of tabs){
         tab.classList.remove('selected')
     }
     selected.classList += ' selected'
+}
+
+function setParallax(){
+    let background = document.getElementById('bg')
+    background.style.top = (window.scrollY / -8) + 'px'
 }
